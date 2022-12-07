ba9fa14af43e1b33cc2a4cb417435cc4f604b5eb
Tomás Fernández <tomasefernandez2001@hotmail.com>
2022-12-07 18:25:04 -0300

    fecha en models, herencia de templates y busqueda de familiares

--- seed_data.py
+++ seed_data.py
@@ -1,6 +1,6 @@
 from ejemplo.models import Familiar
-Familiar(nombre="Rosario", direccion="Rio Parana 745", numero_pasaporte=123123).save()
-Familiar(nombre="Alberto", direccion="Rio Parana 745", numero_pasaporte=890890).save()
-Familiar(nombre="Samuel", direccion="Rio Parana 745", numero_pasaporte=345345).save()
-Familiar(nombre="Florencia", direccion="Rio Parana 745", numero_pasaporte=567567).save()
+Familiar(nombre="Rosario", direccion="Rio Parana 745", fecha="2001-12-01",numero_pasaporte=123123).save()
+Familiar(nombre="Alberto", direccion="Rio Parana 745", fecha="1993-03-15",numero_pasaporte=890890).save()
+Familiar(nombre="Samuel", direccion="Rio Parana 745", fecha="1998-01-29",numero_pasaporte=345345).save()
+Familiar(nombre="Florencia", direccion="Rio Parana 745", fecha="1996-07-19", numero_pasaporte=567567).save()
 print("Se cargo con éxito los usuarios de pruebas")
