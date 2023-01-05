857a592c9c4e786b1ce596a99d9cb60d418aa86d
Tomás Fernández <tomasefernandez2001@hotmail.com>
2023-01-05 18:22:58 -0300

    avatars y mensajes

--- ejemplo_dos/views.py
+++ ejemplo_dos/views.py
@@ -6,11 +6,13 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from ejemplo_dos.forms import UsuarioForm
+from ejemplo_dos.models import Avatar, Post, Mensaje
+from django.contrib.auth.admin import User
 
-@login_required
+
 def index(request):
-    return render(request, "ejemplo_dos/index.html", {})
-
+    posts = Post.objects.order_by("-publicado_el").all()
+    return render(request, "ejemplo_dos/index.html", {"posts": posts})
 
 class PostDetalle(DetailView):
     model = Post
@@ -32,14 +34,41 @@ class PostBorrar(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = reverse_lazy("ejemplo-dos-listar")
 
-
 class UserSignUp(CreateView):
     form_class = UsuarioForm
     template_name = "registration/signup.html"
     success_url = reverse_lazy("ejemplo-dos-listar")
+
 
 class UserLogin(LoginView):
     next_page = reverse_lazy("ejemplo-dos-listar")
 
 class UserLogout(LogoutView):
     next_page = reverse_lazy("ejemplo-dos-listar")
+
+class AvatarActualizar(UpdateView):
+    model = Avatar
+    fields = ["imagen"]
+    success_url = reverse_lazy("ejemplo-dos-listar")
+
+
+class UserActualizar(UpdateView):
+    model = User
+    fields = ["first_name", "last_name", "email"]
+    success_url = reverse_lazy("ejemplo-dos-listar")
+
+
+class MensajeDetalle(LoginRequiredMixin, DetailView):
+    model = Mensaje
+
+class MensajeListar(LoginRequiredMixin, ListView):
+    model = Mensaje
+
+class MensajeCrear(CreateView):
+    model = Mensaje
+    success_url = reverse_lazy("ejemplo-dos-listar")
+    fields = ["nombre", "email", "texto"]
+
+class MensajeBorrar(LoginRequiredMixin, DeleteView):
+    model = Mensaje
+    success_url = reverse_lazy("ejemplo-dos-listar")
--- project/urls.py
+++ project/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView 
@@ -29,7 +31,9 @@ from ejemplo.views import (index, saludar_a, sumar,
                             MascotaDetalle, BuscarMascota, BuscarAuto)
 from ejemplo_dos.views import (index, PostListar, PostCrear, PostBorrar, 
                                 PostActualizar, PostDetalle, UserSignUp,
-                                UserLogin, UserLogout)
+                                UserLogin, UserLogout, AvatarActualizar,
+                                UserActualizar, MensajeCrear, MensajeListar, 
+                                MensajeDetalle)
 from django.contrib.admin.views.decorators import staff_member_required
 
 urlpatterns = [
@@ -39,37 +43,45 @@ urlpatterns = [
     path("sumar/<int:a>/<int:b>/", sumar),
     path("buscar/", buscar),
     path('mi-familia/', mostrar_familiares),
-    path('mi-familia/buscar', BuscarFamiliar.as_view()),
-    path('mi-familia/alta', AltaFamiliar.as_view()),
-    path('mi-familia/actualizar/<int:pk>', ActualizarFamiliar.as_view()),
-    path('mi-familia/borrar/<int:pk>', BorrarFamiliar.as_view()),
+    path('mi-familia/buscar/', BuscarFamiliar.as_view()),
+    path('mi-familia/alta/', AltaFamiliar.as_view()),
+    path('mi-familia/actualizar/<int:pk>/', ActualizarFamiliar.as_view()),
+    path('mi-familia/borrar/<int:pk>/', BorrarFamiliar.as_view()),
     path('panel-familia/', FamiliarList.as_view()),
-    path('panel-familia/crear', FamiliarCrear.as_view()),
-    path('panel-familia/<int:pk>/borrar', FamiliarBorrar.as_view()),
-    path('panel-familia/<int:pk>/actualizar', FamiliarActualizar.as_view()),
+    path('panel-familia/crear/', FamiliarCrear.as_view()),
+    path('panel-familia/<int:pk>/borrar/', FamiliarBorrar.as_view()),
+    path('panel-familia/<int:pk>/actualizar/', FamiliarActualizar.as_view()),
     path('success_updated_message', TemplateView.as_view(template_name="ejemplo/success_updated_message.html")),
-    path('panel-familia/<int:pk>/detalle', FamiliarDetalle.as_view()),
+    path('panel-familia/<int:pk>/detalle/', FamiliarDetalle.as_view()),
     path('panel-auto/', AutoList.as_view()),
-    path('panel-auto/crear', AutoCrear.as_view()),
-    path('panel-auto/<int:pk>/borrar', AutoBorrar.as_view()),
-    path('panel-auto/<int:pk>/actualizar', AutoActualizar.as_view()),
-    path('success_updated_message_auto', TemplateView.as_view(template_name="ejemplo/success_updated_message_auto.html")),
-    path('panel-auto/<int:pk>/detalle', AutoDetalle.as_view()),
+    path('panel-auto/crear/', AutoCrear.as_view()),
+    path('panel-auto/<int:pk>/borrar/', AutoBorrar.as_view()),
+    path('panel-auto/<int:pk>/actualizar/', AutoActualizar.as_view()),
+    path('success_updated_message_auto/', TemplateView.as_view(template_name="ejemplo/success_updated_message_auto.html")),
+    path('panel-auto/<int:pk>/detalle/', AutoDetalle.as_view()),
     path('panel-mascota/', MascotaList.as_view()),
-    path('panel-mascota/crear', MascotaCrear.as_view()),
-    path('panel-mascota/<int:pk>/borrar', MascotaBorrar.as_view()),
-    path('panel-mascota/<int:pk>/actualizar', MascotaActualizar.as_view()),
-    path('success_updated_message_mascota', TemplateView.as_view(template_name="ejemplo/success_updated_message_mascota.html")),
-    path('panel-mascota/<int:pk>/detalle', MascotaDetalle.as_view()),
-    path('panel-mascota/buscar', BuscarMascota.as_view()),
-    path('panel-auto/buscar', BuscarAuto.as_view()),
+    path('panel-mascota/crear/', MascotaCrear.as_view()),
+    path('panel-mascota/<int:pk>/borrar/', MascotaBorrar.as_view()),
+    path('panel-mascota/<int:pk>/actualizar/', MascotaActualizar.as_view()),
+    path('success_updated_message_mascota/', TemplateView.as_view(template_name="ejemplo/success_updated_message_mascota.html")),
+    path('panel-mascota/<int:pk>/detalle/', MascotaDetalle.as_view()),
+    path('panel-mascota/buscar/', BuscarMascota.as_view()),
+    path('panel-auto/buscar/', BuscarAuto.as_view()),
     path('ejemplo-dos/', index, name="ejemplo-dos-index"),
-    path('ejemplo-dos/listar', PostListar.as_view(), name="ejemplo-dos-listar"),
-    path('ejemplo-dos/crear', staff_member_required(PostCrear.as_view()), name="ejemplo-dos-crear"),
-    path('ejemplo-dos/<int:pk>/borrar', staff_member_required(PostBorrar.as_view()), name="ejemplo-dos-borrar"),
-    path('ejemplo-dos/<int:pk>/actualizar', staff_member_required(PostActualizar.as_view()), name="ejemplo-dos-actualizar"),
-    path('ejemplo-dos/<int:pk>/detalle', PostDetalle.as_view(), name="ejemplo-dos-detalle"),
-    path('ejemplo-dos/signup', UserSignUp.as_view(), name="ejemplo-dos-signup"),
-    path('ejemplo-dos/login', UserLogin.as_view(), name="ejemplo-dos-login"),
-    path('ejemplo-dos/logout', UserLogout.as_view(), name="ejemplo-dos-logout"),
+    path('ejemplo-dos/listar/', PostListar.as_view(), name="ejemplo-dos-listar"),
+    path('ejemplo-dos/crear/', staff_member_required(PostCrear.as_view()), name="ejemplo-dos-crear"),
+    path('ejemplo-dos/<int:pk>/borrar/', staff_member_required(PostBorrar.as_view()), name="ejemplo-dos-borrar"),
+    path('ejemplo-dos/<int:pk>/actualizar/', staff_member_required(PostActualizar.as_view()), name="ejemplo-dos-actualizar"),
+    path('ejemplo-dos/<int:pk>/detalle/', PostDetalle.as_view(), name="ejemplo-dos-detalle"),
+    path('ejemplo-dos/signup/', UserSignUp.as_view(), name="ejemplo-dos-signup"),
+    path('ejemplo-dos/login/', UserLogin.as_view(), name="ejemplo-dos-login"),
+    path('ejemplo-dos/logout/', UserLogout.as_view(), name="ejemplo-dos-logout"),
+    path('ejemplo-dos/avatars/<int:pk>/actualizar/', AvatarActualizar.as_view(), name="ejemplo-dos-avatars-actualizar"),
+    path('ejemplo-dos/users/<int:pk>/actualizar/', UserActualizar.as_view(), name="ejemplo-dos-users-actualizar"),
+    path('ejemplo-dos/mensajes/crear/', MensajeCrear.as_view(), name="ejemplo-dos-mensajes-crear"),
+    path('ejemplo-dos/mensajes/listar/', MensajeListar.as_view(), name="ejemplo-dos-mensajes-listar"),
+    path('ejemplo-dos/mensajes/<int:pk>/detalle/', MensajeDetalle.as_view(), name="ejemplo-dos-mensajes-detalle"),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
